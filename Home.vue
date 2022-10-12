8fb8d9fa4a448f4fccc575fbde48b220a067b770
Javier <javierascazuri@gmail.com>
2022-10-12 19:58:37 -0300

    SomeComment

--- Home.vue
+++ Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/pizzorno.jpg" style="max-width: 500px; padding-top: 100px;">
-    <!-- <h3>Pizzorno y Cia</h3> -->
+
   </div>
 </template>
 
